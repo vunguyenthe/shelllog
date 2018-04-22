eb13e8742ab19f53b4cdea58da5386e477105680
Vu Nguyen <vunguyenthe1976@gmail.com>
2018-04-22 09:35:18 +0700

    check asscii

--- doit.sh
+++ doit.sh
@@ -1,46 +1,45 @@
 #!/bin/bash
 declare -r MAX_LEN_PER_LINE=128
-if [ ! -f $1 ]; then
-    echo "File not found!"
-	exit
-elif [ -z "$1" ]; then
-    echo "Command is invalid!"
-	exit	
-fi	
 function isDateValid {
-    DATE=$element
-
+    DATE=$1
+	
+	date_parts=$(echo $DATE | awk -F '.' '{ print NF}')
+	
+	if [ $date_parts -ne 3 ]; then
+		return 1
+	fi
+	
     if [[ $DATE =~ ^[0-9]{1,2}.[0-9a-zA-Z]{1,3}.[0-9]{4}$ ]]; then
         #echo "Date $DATE is a number!"
         day=`echo $DATE | cut -d'.' -f1`
         month=`echo $DATE | cut -d'.' -f2`
         year=`echo $DATE | cut -d'.' -f3`
 
-                if [ "$month" == "01" ] || [ "$month" == "1" ]; then
-                        month="Jan"
-                elif [ "$month" == "02" ] || [ "$month" == "2" ]; then
-                        month="Feb"
-                elif [ "$month" == "03" ] || [ "$month" == "3" ]; then
-                        month="Mar"
-                elif [ "$month" == "04" ] || [ "$month" == "4" ]; then
-                        month="Apr"
-                elif [ "$month" == "05" ] || [ "$month" == "5" ]; then
-                        month="May"
-                elif [ "$month" == "06" ] || [ "$month" == "6" ]; then
-                        month="Jun"
-                elif [ "$month" == "07" ] || [ "$month" == "7" ]; then
-                        month="Jul"
-                elif [ "$month" == "08" ] || [ "$month" == "8" ]; then
-                        month="Aug"
-                elif [ "$month" == "09" ] || [ "$month" == "9" ]; then
-                        month="Sep"
-                elif [ "$month" == "10" ]; then
-                        month="Oct"
-                elif [ "$month" == "11" ]; then
-                        month="Nov"
-                elif [ "$month" == "12" ]; then
-                        month="Dec"
-                fi
+		if [ "$month" == "01" ] || [ "$month" == "1" ]; then
+				month="Jan"
+		elif [ "$month" == "02" ] || [ "$month" == "2" ]; then
+				month="Feb"
+		elif [ "$month" == "03" ] || [ "$month" == "3" ]; then
+				month="Mar"
+		elif [ "$month" == "04" ] || [ "$month" == "4" ]; then
+				month="Apr"
+		elif [ "$month" == "05" ] || [ "$month" == "5" ]; then
+				month="May"
+		elif [ "$month" == "06" ] || [ "$month" == "6" ]; then
+				month="Jun"
+		elif [ "$month" == "07" ] || [ "$month" == "7" ]; then
+				month="Jul"
+		elif [ "$month" == "08" ] || [ "$month" == "8" ]; then
+				month="Aug"
+		elif [ "$month" == "09" ] || [ "$month" == "9" ]; then
+				month="Sep"
+		elif [ "$month" == "10" ]; then
+				month="Oct"
+		elif [ "$month" == "11" ]; then
+				month="Nov"
+		elif [ "$month" == "12" ]; then
+				month="Dec"
+		fi
 
         ymd=$year"-"$month"-"$day
         #echo "ymd: "$ymd
@@ -59,17 +58,27 @@ function isDateValid {
     fi
 }
 function isTimeValid() {
-	TIME=$element
-	ret=$(echo $TIME | awk -F ':' '{ print ($1 <= 23 && $2 <= 59 && $3 <= 59) ? 1 : 0 }')
-	if [ $ret = 1 ]; then
-		return 0
-    else
+	TIME=$1
+	
+	time_parts=$(echo $TIME | awk -F ':' '{ print NF}')
+	
+	if [ $time_parts -ne 3 ]; then
 		return 1
+	fi
+	
+	hour=$(echo $TIME | awk -F ':' '{ print $1}')
+	min=$(echo $TIME | awk -F ':' '{ print $2}')
+	sec=$(echo $TIME | awk -F ':' '{ print $3}' | awk -F '.' '{ print $1}')
+
+	if [ $hour -gt 23 ] || [ $min -gt 59 ] || [ $sec -gt 59 ]; then
+		return 1
+    else
+		return 0
     fi
 }
 function isMetaInfoValid() {
-	META=$line
-	if [[ $META =~ "DEBUG:" || $META =~ "INFO:" || $META =~ "WARN:" || $META =~ "ERROR:" ]]; then
+	META=$1
+	if [[ "$META" == *"DEBUG:"* ]] || [[ "$META" == *"INFO:"* ]] || [[ "$META" == *"WARN:"* ]] || [[ "$META" == *"ERROR:"* ]]; then
 		#echo "matched"
 		return 0;
 	else
@@ -78,13 +87,42 @@ function isMetaInfoValid() {
 	fi
 }
 function isTextLengthValid() {
-	TEXT=$line
+	TEXT=$1
+	
+	for ((i=1;i<5;i++)); do
+		element=$(echo $TEXT | awk -F' ' "{print \$$i}")
+		if [ $i -eq 1 ]; then
+			if isDateValid $element; then
+				TEXT=${TEXT/"$element "/}
+			fi	
+		fi
+		if [ $i -eq 1 ] || [ $i -eq 2 ]; then
+			if isTimeValid $element ; then
+				TEXT=${TEXT/"$element "/}
+			fi
+		fi
+		if isMetaInfoValid $element; then
+			TEXT=${TEXT/"- $element: "/}
+		fi	
+	done
+	
 	iLen=${#TEXT}
 	if [[ iLen -lt $MAX_LEN_PER_LINE ]]; then 
 		return 0
 	else
 		return 1
 	fi
+}
+
+function isTextNotContainsNonASCII() {
+	TEXT=$1
+	
+	if [[ $TEXT = *[![:ascii:]]* ]]; then 
+		return 1
+	else
+		return 0
+	fi
+
 }
 #---------------main function-----------
 mwIdx=0
@@ -93,14 +131,18 @@ DATE_WRONG_LINES=()
 TIME_WORNG_LINES=()
 META_WRONG_LINES=()
 TEXT_TOO_LONG_LINES=()
+TEXT_CONTAINS_NONASCII_LINES=()
 while IFS='' read -r line || [[ -n "$line" ]]; do
 	vals=$(echo $line | tr " " "\n")
 	lineNumber=$((lineNumber + 1))
-	if ! isMetaInfoValid $line; then  
+	if ! isMetaInfoValid "$line"; then  
 		META_WRONG_LINES+=($lineNumber)
 	fi	
-	if ! isTextLengthValid $line; then 
+	if ! isTextLengthValid "$line"; then 
 		TEXT_TOO_LONG_LINES+=($lineNumber)
+	fi
+	if ! isTextNotContainsNonASCII "$line"; then 
+		TEXT_CONTAINS_NONASCII_LINES+=($lineNumber)
 	fi
 	var=0
 	dwIdx=0
@@ -130,30 +172,42 @@ iDateWrongLen=${#DATE_WRONG_LINES[@]}
 iTimeWrongLen=${#TIME_WORNG_LINES[@]}
 iMetaWrongLen=${#META_WRONG_LINES[@]}
 iTextTooLongLen=${#TEXT_TOO_LONG_LINES[@]}
-fname=$(echo $1 | cut -f 1 -d '.') #get only filename without ext
-LOG_FILE="$fname""_""report.log"
-if [[ $iDateWrongLen -gt 0 || $iDateWrongLen -gt 0 ||  $iDateWrongLen -gt 0 || $iDateWrongLen -gt 0 ]]; then
-	echo "--------Failed with summary----------" | tee "${LOG_FILE}" #overwrite if it existed
-	echo "Date: $iDateWrongLen errors"	| tee -a "${LOG_FILE}"
-	echo "Time: $iTimeWrongLen errors" | tee -a "${LOG_FILE}"
-	echo "MetaInfo: $iMetaWrongLen errors"	| tee -a "${LOG_FILE}"
-	echo "Text too long: $iTextTooLongLen errors" | tee -a "${LOG_FILE}"
-	echo "--------Failed with details---------" | tee -a "${LOG_FILE}"
-	echo "Date wrong lines:  ${DATE_WRONG_LINES[@]}" | tee -a "${LOG_FILE}"
-	#time validation
-	echo "Time wrong lines: ${TIME_WORNG_LINES[@]}" | tee -a "${LOG_FILE}"
-	#meta validation
-	echo "Meta wrong lines: ${META_WRONG_LINES[@]}" | tee -a "${LOG_FILE}"
-	#Text validation
-	echo "Text too long lines: ${TEXT_TOO_LONG_LINES[@]}" | tee -a "${LOG_FILE}"
-	echo "Please find ${LOG_FILE} to get a detailed report";
-else 
-	echo "--------Success with summary----------" > "${LOG_FILE}" #overwrite if it existed
-	echo "Date: $iDateWrongLen errors"	| tee -a "${LOG_FILE}"
-	echo "Time: $iTimeWrongLen errors"	| tee -a "${LOG_FILE}"
-	echo "MetaInfo: $iMetaWrongLen errors"	| tee -a "${LOG_FILE}"
-	echo "Text too long: $iTextTooLongLen errors"	| tee -a "${LOG_FILE}"	
-	echo "Please find ${LOG_FILE} to get a detailed report";
-fi
+iTextContainsNonASCII=${#TEXT_CONTAINS_NONASCII_LINES[@]}
 
+if [[ $iDateWrongLen -gt 0 || $iTimeWrongLen -gt 0 ||  $iMetaWrongLen -gt 0 || $iTextTooLongLen -gt 0 || $iTextContainsNonASCII -gt 0 ]]; then
+	echo "--------Failed with summary----------"
+	printf "Date: %s errors\n" "$iDateWrongLen"	
+	printf "Time: %s errors\n" "$iTimeWrongLen"	
+	printf "MetaInfo: %s errors\n" "$iMetaWrongLen"	
+	printf "Text too long: %s errors\n" "$iTextTooLongLen"	
+	printf "Text contains Non-ASCII: %s errors\n" "$iTextContainsNonASCII"	
+
+	echo "--------Failed with details---------"
+	printf 'Date wrong lines: '
+	printf "%s " "${DATE_WRONG_LINES[@]}"	
+	printf "\n"
+	#time validation
+	printf 'Time wrong lines: '
+	printf "%s " "${TIME_WORNG_LINES[@]}"	
+	printf "\n"
+	#meta validation
+	printf 'Meta wrong lines: '
+	printf "%s " "${META_WRONG_LINES[@]}"	
+	printf "\n"
+	#Text validation
+	printf 'Text too long lines: '
+	printf "%s " "${TEXT_TOO_LONG_LINES[@]}"	
+	printf "\n"
+	#Text contains Non-ASCII
+	printf 'Text contains Non-ASCII lines: '
+	printf "%s " "${TEXT_CONTAINS_NONASCII_LINES[@]}"	
+	printf "\n"
+else 
+	echo "--------Success with summary----------"
+	printf "Date: %s errors\n" "$iDateWrongLen"	
+	printf "Time: %s errors\n" "$iTimeWrongLen"	
+	printf "MetaInfo: %s errors\n" "$iMetaWrongLen"	
+	printf "Text too long: %s errors\n" "$iTextTooLongLen"	
+	printf "Text contains Non-ASCII: %s errors\n" "$iTextContainsNonASCII"	
+fi
 
